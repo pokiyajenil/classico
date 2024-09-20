2c118afe772b54a85e1563581612d2cafe1792e9
jenil <jenilpokiya077@gmail.com>
2024-09-20 19:55:44 +0530

    push to github

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,19 +1,15 @@
 import 'package:classico/models/classico.dart';
 import 'package:flutter/material.dart';
 
-abstract class ItemWidget extends StatelessWidget {
-  const ItemWidget({super.key, required this.item});
-     final Item item;
+class ItemWidget extends StatelessWidget {
+  final Item item;
 
-  // ignore: unnecessary_null_comparison
-  const ItemWdiget ({super.key, required this.item}):assert(item !=null){
-   
-   
+  const ItemWidget({super.key, required this.item}) : assert(item != null);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      leading: Image.network(item.image),
+    );
   }
 }
-
-  }
